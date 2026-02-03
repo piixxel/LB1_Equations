884ce37b0d728e5acecfa1040826604517eb6c31
piixxel <vadik.trifonow2015@gmail.com>
2026-02-03 18:49:36 +0300

    Update LB1_Equations.cpp

--- LB1_Equations/LB1_Equations.cpp
+++ LB1_Equations/LB1_Equations.cpp
@@ -3,96 +3,86 @@
 #include <string>
 #include <algorithm>
 
-
 using namespace std;
 
-class Equations
+//валидация
+double getValue(string text)
 {
-public:
-    Equations();
-    void diskr();
-    bool prov_1();
-    void prov_disk();
-
-private:
-    double a, b, c, disk;
-
-};
-//ввод числа с проверкой 
-double getNum(const string text)
-{
+    string value;
+    int length;
+    bool boo;
     
-    double value;
-    cout << text;
     while (true)
     {
+        cout << text;
         cin >> value;
-        if (cin.fail())
+        boo = true;
+        length = value.size();
+
+        for (int i = 0; i < length; i++) //проверка на запятую
         {
-            cout << "Ошибка!!! Озаруешь, КИРИЛЛ :)\n" << text;
-            cin.clear();
-            cin.ignore(1000, '\n');
-            continue;
+            if (value[i] == ',')
+                value[i] = '.';
         }
-        return value;
+
+        for (int i = 0; i < length; i++) //проверка на буквы
+        {
+            if (!((value[i] >= '0' && value[i] <= '9') || value[i] == '.' || value[i] == '-'))
+            {
+                boo = false;
+                cout << "Error! Try again" << endl;
+                break;
+            }
+        }
+
+        if (boo)
+        {
+            break;
+        }
+           
     }
+    return stod(value);
 }
 
-Equations::Equations()
-{
-    
-    cout << "Введите коэффициенты для уравнения ax^2 + bx + c = 0" << endl;
-    this->a = getNum("Введите коэффициент a = ");
-    this->b = getNum("Введите коэффициент b = ");
-    this->c = getNum("Введите коэффициент c = ");
-    this->disk = 0;
-}
 
-void Equations::diskr()
-{
-    this->disk = (pow(b, 2)) - (4 * a * c);
-}
-
-bool Equations::prov_1()
+// проверка аргументов
+bool provDisk(double a, double b, double c) 
 {
     if (a == 0)
     {
         if (b == 0)
         {
-            if (c == 0)
-            {
-                cout << "Нет решений" << endl;
-                return false;
-            }
-            cout << "Бесконечность решений" << endl;
+            (c == 0) ? cout << "No solutions" << endl : cout << "So many solutions" << endl;
             return false;
         }
-        double x;
-        x = -c / b;
-        cout << "Корень уравнения x = " << x << endl;
+        double x = -c / b;
+        cout << "Solution x = " << x << endl;
         return false;
     }
     return true;
 }
 
-void Equations::prov_disk()
+//подсчет корней
+void itog(double a, double b, double c)
 {
-    if (this->disk < 0)
+    double disk = (pow(b, 2)) - (4 * a * c);
+
+    if (disk < 0)
     {
-        cout << "Нет решений" << endl;
+        cout << "No solutions" << endl;
         return;
     }
-    if (this->disk == 0)
+    if (disk == 0)
     {
         double x = (-b) / (2 * a);
-        cout << "Корень уравнения x = " << x << endl;
+        cout << "Solution x = " << x << endl;
         return;
     }
-    if (this->disk > 0)
+    if (disk > 0)
     {
         double x1 = (-b - sqrt(disk)) / (2 * a);
         double x2 = (-b + sqrt(disk)) / (2 * a);
-        cout << "Корни уравнения  x1 = " << x1 << " и x2 = " << x2 << endl;
+        cout << "Solutions  x1 = " << x1 << " и x2 = " << x2 << endl;
         return;
     }
 }
@@ -101,13 +91,16 @@ void Equations::prov_disk()
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
-    Equations a;
-    
-    if (!a.prov_1())
+   
+    double a = getValue("Enter value a = ");
+    double b = getValue("Enter value b = ");
+    double c = getValue("Enter value c = ");
+    if (!provDisk(a, b, c))
+    {
         return 0;
-    a.diskr();
-    a.prov_disk();
+    }
+       
+    itog(a,b,c);
 
     return 0;
 }
